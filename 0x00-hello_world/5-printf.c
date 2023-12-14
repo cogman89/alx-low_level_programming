15ee7c343881ce1c5a938c5b5347885eb71ee302
Dave M <mureithidave@gmail.com>
2023-12-14 04:05:25 -0800

    5-printf.c hotfix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  *
  * Program prints with proper grammar, but the outcome is a piece of art
  */
-int main
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
