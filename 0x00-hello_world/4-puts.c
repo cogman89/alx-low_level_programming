134427f73445553c25f4dda28e08e18920595548
Dave M <mureithidave@gmail.com>
2023-12-14 03:29:11 -0800

    C program that prints an output, followed by a new line.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main  is the Entry point
+ *
+ * the program prints exactly "Programming is like building a multilingual puzzle, followed by a new line."
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
